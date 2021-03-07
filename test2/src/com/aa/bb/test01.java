366ca1936e6620e4a81aeb9572131f2af283c392
ttlszteam <541701832@qq.com>
2021-03-07 15:37:36 +0800

    99

--- test2/src/com/aa/bb/test01.java
+++ test2/src/com/aa/bb/test01.java
@@ -3,7 +3,7 @@ package com.aa.bb;
 public class test01 {
 
 	public static void main(String[] args) {
-		System.out.println("qaaaa");
+		System.out.println("AAAA");
 		int i=0;
 		System.out.println(i);  
 	}
